8d3c41f7790d179f5c79dc83162dc02419dd7c9b
Parth Sharma <parthsharma151@gmail.com>
2021-05-15 13:37:22 +0530

    add: contact-card section

---
+++ bc-client/src/components/ContactCard.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="wrapper">
+        <div class="contact-card">
+            <img :src="imgSrc" class="contact-img" >
+            <div class="contact-name">{{ name }}</div>
+            <div class="contact-desig">{{ designation }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ContactCard",
+    props: [
+        "imgSrc",
+        "name",
+        "designation"
+    ]
+}
+</script>
+
+<style scoped>
+    .wrapper {
+        margin-right: 20px;
+    }
+    .contact-card {
+        height: 100%;
+        width: 100%;
+        background: #2f2f2f;
+        border-radius: 15px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        transition: transform 300ms;
+    }
+    .contact-card:hover {
+        transform: translateY(-5px);
+    }
+    .contact-name {
+        font-size: 24px;
+        color: white;
+        font-weight: 500;
+    }
+    .contact-desig {
+        font-size: 20px;
+        color: white;
+        font-weight: 400;
+        padding-bottom: 20px;
+    }
+    .contact-img {
+        height: 200px;
+        width: 180px;
+        padding: 20px;
+        border-radius: 35px;
+        object-fit: cover;
+    }
+</style>
