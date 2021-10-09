2d98c495860df6396814434f6ce70192fd28c2cf
sarthak arora <f20200060@pilani.bits-pilani.ac.in>
2021-10-09 11:15:44 +0530

    fixes

--- bc-client/src/views/Club.vue
+++ bc-client/src/views/Club.vue
@@ -78,16 +78,16 @@
     <div class="club-calendar">
       <h2 class="club-subh" id="media">Upcoming Events</h2>
       <div class="cal-grid">
-        <!-- <div v-for="calItem in club.events" :key="calItem.id">
+        <div v-for="calItem in club.events" :key="calItem.id">
           <CalendarItem
-            :imgSrc="'https://clubs.bits-dvm.org/assets/'+ item.events_id.eventfeatureimage"
+            :imgSrc="'https://clubs.bits-dvm.org/assets/'+ calItem.events_id.eventfeatureimage"
             :eventName="calItem.events_id.eventname"
             :date="calItem.events_id.eventdatetime"
             :link="calItem.events_id.eventlink"
             :description="calItem.events_id.eventdesc"
             :modalName="calItem.events_id.eventname"
           />
-        </div> -->
+        </div>
       </div>
     </div>
 
@@ -167,7 +167,7 @@ export default {
         "https://clubs.bits-dvm.org/items/clubs/" + this.id+ '?fields[]=*.*.*'
       );
       this.club = response.data.data;
-      console.log(this.club);
+      console.log(this.club.events);
       // console.log(this.club.events[1].id);
     } catch (error) {
       this.error = error;
